f160bb7d66db879574cbf279b212c4e34861033f
SEEKASIA\cyyong <cyyong@seekasia.com>
2017-05-17 12:34:50 +0800

    resolve conflict

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,4 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import './Card.css';
-
-export default class Card extends Component {
-    render() {
-        const {children, extraTopMargin, className} =  this.props;
-
-        return(
-            <div className={`background-card ${extraTopMargin ? 'extraTopMargin' : ''} ${className}`}>
-                {children}
-            </div>
-        );
-    }
-}
-=======
-
 import './Card.css';
 
 export default class Card extends Component {
@@ -52,4 +36,3 @@ export default class Card extends Component {
 
     }
 }
->>>>>>> 8a85f8a6400a984bf5ab810671d4d36f074db37a
