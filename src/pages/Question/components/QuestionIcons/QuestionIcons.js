2f61225b13667c88ad4001c360df46e7eb7c667a
syakir <syakiru@seekasia.com>
2017-05-18 11:44:12 +0800

    Fix questions out of range

--- src/pages/Question/components/QuestionIcons/QuestionIcons.js
+++ src/pages/Question/components/QuestionIcons/QuestionIcons.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function QuestionConfirmation ({icons, qKey = -1, answers = [], onClick = () => {}}) {
+const icons = [{
+    header: 'Strongly Disagree',
+    inactive: 'thumb-down-grey',
+    active: 'thumb-down-red',
+}, {
+    inactive: 'a-bit-no-grey',
+    active: 'a-bit-no-red'
+}, {
+    header: 'Neither Agree or Disagree',
+    inactive: 'whatever-sign-grey',
+    active: 'whatever-sign-yellow'
+}, {
+    inactive: 'a-bit-yes-grey',
+    active: 'a-bit-yes-green'
+}, {
+    header: 'Strongly Agree',
+    inactive: 'thumb-up-grey',
+    active: 'thumb-up-green'
+}];
+
+export default function QuestionConfirmation ({
+    qKey,
+    showHeaders = false,
+    answers = [],
+    onClick = () => {}
+}) {
     return (
         <div className='answersContainer'>
             {icons.map((icon, aKey) => (
                 <div className='answer clickable' key={aKey}>
+                    {showHeaders ? <h1>{icon.header || ''}</h1> : null}
                     <img
                         className='image'
                         alt='thumb'
@@ -19,8 +45,8 @@ export default function QuestionConfirmation ({icons, qKey = -1, answers = [], o
 }
 
 QuestionConfirmation.propTypes = {
-    icons: PropTypes.array,
     qKey: PropTypes.number,
     onClick: PropTypes.func,
-    answers: PropTypes.array
+    answers: PropTypes.array,
+    showHeaders: PropTypes.bool
 };
