92d5b1c6f3eb66b83faf070be386d8621dc866ba
syakir <syakiru@seekasia.com>
2017-05-18 08:30:02 +0800

    Update link in home and nudge

--- src/components/Nudge/Nudge.js
+++ src/components/Nudge/Nudge.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react';
 import Card from '../Card/Card';
-import Logo from '../Logo/Logo'
+import Logo from '../Logo/Logo';
+import { Link } from 'react-router';
 
 import './Nudge.css';
 
 export default class Nudge extends Component {
-    render() {
+    render () {
         return (
           <div className='nudge'>
               <Card closeButton>
                 <div className='logoContainer'>
-                    <Logo className="extra-small-icon"/>
+                    <Logo className='extra-small-icon'/>
                 </div>
                 <div className='nudgeContent'>
                     <div className='nudgeTitle'>Wanna know your personality skill?</div>
                     <div className='nudgeTitle'>Leading you to the more success future</div>
                     <div className='btnWrapper'>
-                        <button className='btn btnFillUp'>Fill up for free now</button>
+                        <Link to='questions/1' className='btn btnFillUp'>Fill up for free now</Link>
                     </div>
                 </div>
               </Card>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,20 +6,20 @@ import Card from '../../components/Card/Card';
 import Button from  '../../components/Button/Button';
 
 export default class Home extends Component {
-    render() {
+    render () {
         return (
-            <div className="background">
-                <Card className="background-yellow padding-width">
+            <div className='background'>
+                <Card className='background-yellow padding-width'>
                     Hello there!
                     Do take some time to complete the following personality test and find out what you are like as a person and your work-related personality! This will greatly enhance your profile to showcase to potential employees too!
                     In this test, there are no right or wrong answers. Simply answer in a way that is most like you.
                 </Card>
-                <Card extraTopMargin className="padding-width-percentage-10">
-                    <Logo className="small-icon" />
+                <Card extraTopMargin className='padding-width-percentage-10'>
+                    <Logo className='small-icon' />
                     <p>16PF personality test is used by employers for employment, promotion, coaching, and career development. Completing this test will greatly increase your chances of winning that dream job!</p>
                 </Card>
-                <Card className="background-light-grey card-flex-center padding-large" extraTopMargin>
-                    <Button url="#">Let's start</Button>
+                <Card className='background-light-grey card-flex-center padding-large' extraTopMargin>
+                    <Button url='questions/1'>{`Let's start`}</Button>
                 </Card>
             </div>
         );
