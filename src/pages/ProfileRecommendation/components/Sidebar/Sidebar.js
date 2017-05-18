c9bb1b8c5d52c82c752be2eaad06e5d00b1fe08c
SEEKASIA\cyyong <cyyong@seekasia.com>
2017-05-18 15:24:43 +0800

    remove the width for the sidebar

--- src/pages/ProfileRecommendation/components/Sidebar/Sidebar.js
+++ src/pages/ProfileRecommendation/components/Sidebar/Sidebar.js
@@ -4,7 +4,6 @@ export default class Sidebar extends Component {
     render () {
         return (
             <img
-                style={{width: '100%'}}
                 src='/images/jobstreet/profile_side_bar.jpg'
                 alt=''
             />
